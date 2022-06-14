a7b6075faa7900579878507efd710e10da3be185
samepant <sampanter@gmail.com>
2022-06-14 17:38:41 -0400

    appease linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import { KeplrChainDefinition } from "./index";
 import "./App.css";
